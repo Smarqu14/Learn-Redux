072f2f95689d61e48aa0fd27cb60979eedf30c28
Steve Marquez <steveantonio00@gmail.com>
2021-03-03 18:05:51 -0800

    Implement redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions/index';
 
 function App() {
+  // To get the state
+  const counter = useSelector((state) => state.counterReducer);
+  const isLogged = useSelector((state) => state.loggedReducer);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable information I should see</h3> : ''}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/index';
+import { Provider } from 'react-redux';
+// Store -> Globalized state
+
+// Action Increment
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT',
+//   };
+// };
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT',
+//   };
+// };
+
+// // Reducer
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
+
+// let store = createStore(counter);
+// // Display it on the console
+// // This gets the state
+// store.subscribe(() => console.log(store.getState()));
+
+// // Dispatch
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+// //Dispatch tis action to the reducer, reduer checks what to do
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
